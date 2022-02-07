61c712c4d22e034c9d75570047e3c2fe4a8e8f63
hui wang wang <66718889@qq.com>
2022-02-07 11:02:12 -0600

    add respCorr_2dHBHE_E50_truncate_mini.root

--- DLPHIN_code/DLPHIN_cfi.py
+++ DLPHIN_code/DLPHIN_cfi.py
@@ -5,9 +5,9 @@ DLPHIN_pb_folder = "%s/src/HCAL/DLPHIN_pb/" % os.environ['CMSSW_BASE']
 
 DLPHINConfig = cms.PSet(
     DLPHIN_print_config = cms.bool(True),
-    DLPHIN_apply_respCorr = cms.bool(False),
-    DLPHIN_respCorr_name = cms.string(DLPHIN_pb_folder + "JP_respCorr_DLPHIN_zeroOut.root"),
-    DLPHIN_truncate = cms.bool(False),
+    DLPHIN_apply_respCorr = cms.bool(True),
+    DLPHIN_respCorr_name = cms.string(DLPHIN_pb_folder + "respCorr_2dHBHE_E50_truncate_mini.root"),
+    DLPHIN_truncate = cms.bool(True),
     DLPHIN_pb_2dHB = cms.string(DLPHIN_pb_folder + "model_2dHB_p1TeV_cut0_epoch100_weight0.pb"),
     DLPHIN_pb_2dHE = cms.string(DLPHIN_pb_folder + "model_2dHE_p1TeV_cut0p1_epoch100_weight_0to1.pb"),
 )
